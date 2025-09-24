75ad272a52a48c656a26904a1f7049fc2fc4cf6b
Ramadevi-2005 <157826221+Ramadevi-2005@users.noreply.github.com>
2025-09-24 10:58:28 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://ec2-13-60-62-117.eu-north-1.compute.amazonaws.com:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http://ec2-13-62-127-189.eu-north-1.compute.amazonaws.com:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
